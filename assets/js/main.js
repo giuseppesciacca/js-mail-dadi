b7b8bb985ef34f41607750b63eccb002650a5cfc
giuseppesciacca <sciaccagi@gmail.com>
2023-03-13 17:55:36 +0100

    [add] dice game

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,26 @@ submit.addEventListener('click', function (e) {
     };
 })
 
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+ */
+
+const nums = ['1', '2', '3', '4', '5', '6'];
+
+const numBot = nums[Math.floor(Math.random() * nums.length)];
+console.log(numBot);
+
+const numUser = nums[Math.floor(Math.random() * nums.length)];
+console.log(numUser);
+
+if (numBot > numUser) {
+    console.log('Hai perso!');
+} else if (numBot == numUser) {
+    console.log('Stesso numero');
+} else {
+    console.log('Hai vinto!');
+}
+
 
 
 
